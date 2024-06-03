8114b6d11643c519b26f345ae307045399bcdcd0
Jim van Leeuwen <jim.vanleeuwen@student.hu.nl>
2024-06-03 15:25:58 +0200

    Add margin in gallery if browser is safari

--- a4d/statics/js/gallery.js
+++ a4d/statics/js/gallery.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
     gridGallery({
         selector: ".gg-box",
         darkMode: false,
@@ -7,5 +7,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
         rowHeight: 200,
         columnWidth: 200,
     });
+
+    var isSafari = navigator.vendor && navigator.vendor.indexOf('Apple') > -1 &&
+        navigator.userAgent &&
+        navigator.userAgent.indexOf('CriOS') == -1 &&
+        navigator.userAgent.indexOf('FxiOS') == -1;
+
+    if (isSafari) {
+        var images = document.getElementsByClassName('gallery_image')
+        for (var i = 0; i < images.length; i++) {
+            images[i].style.margin = '2px';
+        }
+    }
+
 });
 
